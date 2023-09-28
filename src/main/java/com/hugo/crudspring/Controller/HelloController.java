9e8e198700f5aeedfff3ca9301a07f71c6ac6403
Hugomes99 <hugochristian99@gmail.com>
2023-09-28 19:03:04 -0300

    Hello World spring

---
+++ src/main/java/com/hugo/crudspring/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.hugo.crudspring.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping
+    public @ResponseBody String hello() {
+        return "Hello, World!";
+    }
+}
