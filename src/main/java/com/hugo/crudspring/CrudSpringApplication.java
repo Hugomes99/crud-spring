f0ab50b7c684527dc239a106281197c35bc127af
Hugomes99 <hugochristian99@gmail.com>
2023-09-28 21:37:50 -0300

    API GET HTTP

--- src/main/java/com/hugo/crudspring/CrudSpringApplication.java
+++ src/main/java/com/hugo/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.hugo.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hugo.crudspring.model.Course;
+import com.hugo.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,16 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("front-end");
+
+			courseRepository.save(new Course());
+		};
+	}
 }
